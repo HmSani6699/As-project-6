c41c37056f68ce0a51f2fad0b29da194c3c95a4d
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-04 11:04:22 +0600

    open modal to display features part complete

--- js/app.js
+++ js/app.js
@@ -97,4 +97,74 @@ const loadDetailsData = async (itemId) => {
 
 const displayDetails = (data) => {
     console.log(data)
+    const modalContainer = document.getElementById('modal-card-section');
+    modalContainer.innerHTML = `
+<div>
+    <div
+        class="card w-full bg-red-50 shadow-sm border border-red-400">
+        <div
+            class="card-body p-3">
+            <h2 class="card-title">
+                ${data.description}
+            </h2>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
+                <div>
+                    <p class=" font-semibold text-green-500 p-4 text-center"> ${data.pricing[0].price ? data.pricing[0].price : 'free of cost/'} ${data.pricing[0].plan}</p>
+                </div>
+
+                <div>
+                     <p class=" font-semibold text-red-500 p-4 text-center">${data.pricing[1].price ? data.pricing[1].price : 'free of cost/'} ${data.pricing[1].plan}</p>
+                </div>
+
+                <div>
+                     <p class=" font-semibold text-amber-500 p-4 text-center">${data.pricing[2].price ? data.pricing[2].price : 'free of cost/'} ${data.pricing[2].plan}</p>
+                </div>
+            </div> 
+            <div class="grid grid-cols-1 lg:grid-cols-2 mt-3">
+                <div>
+                  <h2 class="text-2xl font-semibold">Features</h2>
+                  <ul id="features-container" class="list-disc pl-4"></ul>
+                </div>   
+                <div>
+                   <h2 class="text-2xl font-semibold">Integrations</h2>
+                   <ul id="integration-container"></ul>
+                </div> 
+            </div>  
+        </div>
+    </div>
+</div>
+<div>
+    <div
+        class="card w-full bg-base-100 shadow-sm border">
+        <figure class="p-4"><img class="rounded-xl"
+                src="${data.image_link[0]}"
+                alt="Shoes" style="height:230px" />
+        </figure>
+        <div
+            class="card-body p-0 p-4 text-center">
+            <h2
+                class="text-2xl font-semibold text-center">
+                ${data.input_output_examples[0].input ? data.input_output_examples[0].input : 'Can you give any example?'}
+            </h2>
+            <p>
+            ${data.input_output_examples[0].output ? data.input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}
+            </p>
+        </div>
+    </div>
+</div>
+`;
+    //features part
+    const featuresContainer = document.getElementById('features-container');
+    if (data.features == {}) {
+        const p = document.createElement(p);
+        p.innerText = 'No data Found';
+        featuresContainer.appendChild(p)
+    }
+    else {
+        for (const item in data.features) {
+            const li = document.createElement('li')
+            li.innerText = data.features[`${item}`].feature_name;
+            featuresContainer.appendChild(li)
+        }
+    }
 }
